c531828c21fab0ed7bbb876e75bae6fb95950f51
mario32111 <marioge44@gmail.com>
2025-12-04 08:27:35 -0600

    ejemplo notificacion

--- EjemploNotificacion/app/src/main/java/com/example/ejemplonotificacion/MainActivity.kt
+++ EjemploNotificacion/app/src/main/java/com/example/ejemplonotificacion/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.ejemplonotificacion
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.os.Bundle
@@ -55,11 +57,26 @@ class MainActivity : AppCompatActivity() {
     private fun generarNotificacion(){
         val chanelId = "bassic channel"
         val notifiIcon = BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher_round)
+
+        val imagen = BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher_round)
+        val intent = Intent(this@MainActivity, MainActivity2::class.java)
+        intent.putExtra("imagen", R.mipmap.ic_launcher_round)
+
+        val pendingIntent = PendingIntent.getActivity(
+            this@MainActivity,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
         val notificacion = NotificationCompat.Builder(this@MainActivity, chanelId)
             .setSmallIcon(R.mipmap.ic_launcher_round)
             .setLargeIcon(notifiIcon)
-            .setContentTitle("Mensaje importante")
+            .setContentTitle(edt1.text.toString())
             .setContentText("informatica.com.mx")
+            .setStyle(NotificationCompat.BigPictureStyle().bigPicture(imagen))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
             .setSubText("Sitio Web")
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .build()
@@ -70,6 +87,10 @@ class MainActivity : AppCompatActivity() {
                     android.Manifest.permission.POST_NOTIFICATIONS
                 ) != PackageManager.PERMISSION_GRANTED
             ) {
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
+                    1)
                 return
             }
 
---
+++ EjemploNotificacion/app/src/main/java/com/example/ejemplonotificacion/MainActivity2.kt
@@ -0,0 +1,27 @@
+package com.example.ejemplonotificacion
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    lateinit var  img1: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        img1 = findViewById(R.id.imageView1)
+        img1.setImageResource(intent.getIntExtra("imagen", 0))
+
+    }
+}
