b900b84d323c65950ac4c6fa206334c4a29ea4c2
mario32111 <marioge44@gmail.com>
2025-12-10 18:11:40 -0600

    add picasso library

--- PraticaAgenda/app/build.gradle.kts
+++ PraticaAgenda/app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 dependencies {
 
     implementation(libs.volley)
+    implementation("com.squareup.picasso:picasso:2.71828")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
